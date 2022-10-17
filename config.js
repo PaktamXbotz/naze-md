4d3c359cb22ea386938dbe982be36385a47e9cc3
PaktamXbotz <107012758+PaktamXbotz@users.noreply.github.com>
2022-10-17 17:27:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['601169655956']
+global.ownernomer = "601169655956"
+global.premium = ['601169655956']
+global.packname = 'Sticker  NIzxm'
+global.author = 'NIZXM'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
